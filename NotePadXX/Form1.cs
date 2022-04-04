656b898a3ff4a6f59ef95fbf8032645803457dfe
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-04-04 17:57:50 +0300

    toolbar2

--- NotePadXX/Form1.cs
+++ NotePadXX/Form1.cs
@@ -24,6 +24,7 @@ namespace NotePadXX
             menu = new MenuStrip();
             ToolStripMenuItem file = (ToolStripMenuItem)menu.Items.Add("File");
             ToolStripMenuItem edit = (ToolStripMenuItem)menu.Items.Add("Edit");
+            //menu.Location = new Point(100,100);
             this.MainMenuStrip = menu;
             menu.BackColor = Color.Coral;
             this.Controls.Add(menu);
@@ -41,36 +42,38 @@ namespace NotePadXX
 
             this.Controls.Add(tabCtrl);
             this.tabCtrl.Size = this.ClientSize;
-            this.tabCtrl.Location = new Point(5, 20);
+            this.tabCtrl.Location = new Point(5, 90);
             this.tabCtrl.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             this.tabCtrl.Controls.Add(new C_Tab("RMZS"));
             this.tabCtrl.Controls.Add(new C_Tab("ZBS"));
 
             //toolBar
-            //imList = new ImageList();
-            //imList.ImageSize = new Size(50, 50);
-            //imList.Images.Add(new Bitmap("image.bmp"));
-            //imList.Images.Add(new Bitmap("image.bmp"));
-            //imList.Images.Add(new Bitmap("image.bmp"));
-            //tBar = new ToolBar();
-            //tBar.ImageList = imList;
-            //ToolBarButton toolBarButton1 = new ToolBarButton();
-            //ToolBarButton toolBarButton2 = new ToolBarButton();
-            //ToolBarButton toolBarButton3 = new ToolBarButton();
-            //ToolBarButton tbSeparator = new ToolBarButton();
-            //tbSeparator.Style = ToolBarButtonStyle.Separator;
-            //toolBarButton1.ImageIndex = 0;
-            //toolBarButton2.ImageIndex = 1;
-            //toolBarButton3.ImageIndex = 2;
-            //tBar.Buttons.Add(toolBarButton1);
-            //tBar.Buttons.Add(tbSeparator);
-            //tBar.Buttons.Add(toolBarButton2);
-            //tBar.Buttons.Add(tbSeparator);
-            //tBar.Buttons.Add(toolBarButton3);
-            //tBar.Appearance = ToolBarAppearance.Flat;
-            //tBar.BorderStyle = BorderStyle.Fixed3D;
+            imList = new ImageList();
+            imList.ImageSize = new Size(30, 30);
+            imList.Images.Add(new Bitmap("image.bmp"));
+            imList.Images.Add(new Bitmap("image.bmp"));
+            imList.Images.Add(new Bitmap("image.bmp"));
+            tBar = new ToolBar();
+            tBar.ImageList = imList;
+            ToolBarButton toolBarButton1 = new ToolBarButton();
+            ToolBarButton toolBarButton2 = new ToolBarButton();
+            ToolBarButton toolBarButton3 = new ToolBarButton();
+            ToolBarButton tbSeparator = new ToolBarButton();
+            tbSeparator.Style = ToolBarButtonStyle.Separator;
+            toolBarButton1.ImageIndex = 0;
+            toolBarButton2.ImageIndex = 1;
+            toolBarButton3.ImageIndex = 2;
+            tBar.Buttons.Add(toolBarButton1);
+            tBar.Buttons.Add(tbSeparator);
+            tBar.Buttons.Add(toolBarButton2);
+            tBar.Buttons.Add(tbSeparator);
+            tBar.Buttons.Add(toolBarButton3);
+            tBar.Appearance = ToolBarAppearance.Flat;
+            tBar.BorderStyle = BorderStyle.Fixed3D;
+            //tBar.Location = new Point(50, 50);
+            this.Controls.Add(tBar);
+           
 
-            //this.Controls.Add(tBar);
 
         }
         void close_Click(object sender, System.EventArgs e) { this.Close(); }
