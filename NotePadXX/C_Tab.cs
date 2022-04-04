19f247413bf8ff9f465ae606cb8e67ee07be2b19
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-04-04 13:11:32 +0300

    Добавьте файлы проекта.

---
+++ NotePadXX/C_Tab.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+namespace NotePadXX
+{
+    class C_Tab:TabPage
+    {
+        public C_Tab(string title)
+        {
+            this.Text = title;
+            this.Controls.Add(new TextBox() { Multiline = true, Size = ClientSize, Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right });
+        }
+    }
+}
